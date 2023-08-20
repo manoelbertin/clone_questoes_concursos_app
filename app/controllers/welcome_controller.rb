8fd82aedc6125524d7da57c3ccaabdd0043ed56d
manoel bertin <manoeltirj@gmail.com>
2023-08-20 17:43:23 -0300

    Devise configurated and installed in my app.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  http_basic_authenticate_with name: 'manoel', password: '123456'
+  # http_basic_authenticate_with name: 'manoel', password: '123456'
   def index
   end
 end
